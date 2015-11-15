3a76e3d6586b49bcf3cd8a5163e0795294310363
sakusimu <sakusimu@gmail.com>
2015-11-15 21:25:15 +0900

    change to use context from data in deco/assembler

--- test/unit/deco/assembler.js
+++ test/unit/deco/assembler.js
@@ -1,17 +1,27 @@
 'use strict';
 const assert = require('power-assert');
 const Assembler = require('../../../lib/deco/assembler');
+const Context = require('../../../lib/context');
 
 describe('deco/assembler', () => {
+    let context = new Context();
+
     describe('constructor()', () => {
         it('should create assembler', () => {
-            let a = new Assembler();
+            let a = new Assembler(context);
             assert(a);
+            assert(a.context === context);
+        });
+
+        it('should throw exception if no arguments', () => {
+            let got;
+            try { new Assembler(); } catch (e) { got = e.message; }
+            assert(got === 'context is required');
         });
     });
 
     describe('assemble()', () => {
-        let a = new Assembler();
+        let a = new Assembler(context);
 
         it('should return [] if null or etc', () => {
             assert.deepEqual(a.assemble(), []);
