4bd17947c00dfe3790bc97b6e16e5a7a36476ec4
sakusimu <sakusimu@gmail.com>
2015-11-23 22:00:31 +0900

    improve equip/combinator performance
    improve +52% equip/combinator performance in V8
    where [ 斬れ味レベル+1, 高級耳栓, 砥石使用高速化 ] in benchmark

--- test/unit/util/index.js
+++ test/unit/util/index.js
@@ -12,4 +12,24 @@ describe('util/index', () => {
             assert(typeof util.BorderLine === 'function');
         });
     });
+
+    describe('clone()', () => {
+        it('should return clone object', function () {
+            let got = util.clone({ a: 1, b: 2 });
+            let exp = { a: 1, b: 2 };
+            assert.deepEqual(got, exp);
+
+            got = util.clone({});
+            exp = {};
+            assert.deepEqual(got, exp);
+
+            got = util.clone(null);
+            exp = null;
+            assert.deepEqual(got, exp);
+
+            got = util.clone();
+            exp = null;
+            assert.deepEqual(got, exp);
+        });
+    });
 });
